8a057e592f7dc2294118bcd9a0c2b74356f6ac47
RRavn <rene.ravn@kickpower.dk>
2020-03-22 08:40:01 +0100

    2.00 OAuth2 implementation and a Selenium test build

--- TimeRegistrationService/application/src/main/java/dk/rr/services/timeregistrationservice/swagger/SwaggerConfig.java
+++ TimeRegistrationService/application/src/main/java/dk/rr/services/timeregistrationservice/swagger/SwaggerConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
+import static org.springframework.security.oauth2.common.util.OAuth2Utils.CLIENT_ID;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
@@ -24,6 +30,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(regex("/.*"))
                 .build()
+                .securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(metaData());
     }
 
@@ -34,4 +42,42 @@ public class SwaggerConfig {
                 .build();
     }
 
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .scopeSeparator(" ")
+                .useBasicAuthenticationWithAccessCodeGrant(true)
+                .build();
+    }
+
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new AuthorizationCodeGrantBuilder()
+                .tokenEndpoint(new TokenEndpoint("/token", "oauthtoken"))
+                .tokenRequestEndpoint(
+                        new TokenRequestEndpoint(AUTH_SERVER + "/authorize", CLIENT_ID, CLIENT_SECRET))
+                .build();
+
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+                .grantTypes(Arrays.asList(grantType))
+                .scopes(Arrays.asList(scopes()))
+                .build();
+        return oauth;
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(
+                        Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+                .forPaths(PathSelectors.regex("/foos.*"))
+                .build();
+    }
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations"),
+                new AuthorizationScope("foo", "Access foo API") };
+        return scopes;
+    }
 }
--- TimeRegistrationService/run_db.sh
+++ TimeRegistrationService/run_db.sh
@@ -2,7 +2,8 @@
 #
 docker stop SQLServer
 docker rm SQLServer
-docker pull mcr.microsoft.com/mssql/server
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passW0rd' -e 'MSSQL_PID=Express' -p 1433:1433 --name SQLServer -d mcr.microsoft.com/mssql/server:latest
+docker pull mcr.microsoft.com/mssql/server:2017-latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=passW0rd' -e 'MSSQL_PID=Express' -p 1433:1433 --name SQLServer -d mcr.microsoft.com/mssql/server:2017-latest
 sleep 30
-docker exec -it SQLServer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P passW0rd -q 'CREATE DATABASE timeregistration;'
+docker exec -it SQLServer /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P passW0rd
+# -q 'CREATE DATABASE timeregistration;'
